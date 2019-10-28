e33bfa47109793268c596510f186adb9ae2f061c
nunezrio <nunezrio@gmail.com>
2019-10-28 09:04:06 -0400

    replaced var with let

--- script.js
+++ script.js
@@ -14,32 +14,31 @@ function removeElement() {
   document.getElementById("remove-onclick").innerHTML = "";
 }
 
-function showTime(){
-  var date = new Date();
-  var h = date.getHours(); // 0 - 23
-  var m = date.getMinutes(); // 0 - 59
-  var s = date.getSeconds(); // 0 - 59
-  var session = "AM";
-  
-  if(h == 0){
-      h = 12;
+function showTime() {
+  let date = new Date();
+  let h = date.getHours(); // 0 - 23
+  let m = date.getMinutes(); // 0 - 59
+  let s = date.getSeconds(); // 0 - 59
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
   }
-  
-  if(h > 12){
-      h = h - 12;
-      session = "PM";
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
   }
-  
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
-  
-  var time = h + ":" + m + ":" + s + " " + session;
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = h + ":" + m + ":" + s + " " + session;
   document.getElementById("MyClockDisplay").innerText = time;
   document.getElementById("MyClockDisplay").textContent = time;
-  
+
   setTimeout(showTime, 1000);
-  
 }
 
 showTime();
